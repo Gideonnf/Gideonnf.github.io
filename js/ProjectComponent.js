139384005ddc2f6075ace50ba9f029ef80d7a8a6
gideon <gideonnfrancis999@gmail.com>
2020-03-13 09:28:09 +0800

    removed react

---
+++ js/ProjectComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ProjectComponent extends Component {
+    render() {
+        return 
+        (
+
+        )
+    }
+};
+
+export default ProjectComponent;
